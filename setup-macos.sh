aecc274358ed63f43fde4c81f17085707c1ed018
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2021-05-05 21:39:56 +0300

    [Update] macOS setup script documentation

--- setup-macos.sh
+++ setup-macos.sh
@@ -1,12 +1,22 @@
-# brew (requires Xcode)
+#*brew
+# Requires Xcode
+# Install homebrew and all the packages in Brewfile
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew bundle --file=./macos/Brewfile
 
-# git
+
+#*git
+# Configure git
 ln -sf $(pwd)/common/git/.gitconfig ~/.gitconfig
 
-# zsh
+
+#*zsh
+# Install oh-my-zsh and powerlevel10k
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Configure zsh
 ln -sf $(pwd)/common/zsh/.zshrc ~/.zshrc
+
+# Change default shell to zsh
 chsh -s /bin/zsh
