6e6546c7cd51ae56491acf0201b6fbaeae66bd29
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2021-05-05 20:43:47 +0300

    [Add] git configuration
    Git configuration.

--- setup-macos.sh
+++ setup-macos.sh
@@ -1,3 +1,6 @@
+# git
+ln -sf $(pwd)/common/git/.gitconfig ~/.gitconfig
+
 # brew (requires Xcode)
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew bundle --file=./macos/Brewfile
