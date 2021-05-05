2ab25f9b0cd66b6a6c34ed4c10d0b166ecbb6f70
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2021-05-05 21:30:41 +0300

    [Add] zsh configuration
    zsh, oh-my-zsh and powerlevel10k config and setup.

--- setup-macos.sh
+++ setup-macos.sh
@@ -1,6 +1,12 @@
-# git
-ln -sf $(pwd)/common/git/.gitconfig ~/.gitconfig
-
 # brew (requires Xcode)
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew bundle --file=./macos/Brewfile
+
+# git
+ln -sf $(pwd)/common/git/.gitconfig ~/.gitconfig
+
+# zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+ln -sf $(pwd)/common/zsh/.zshrc ~/.zshrc
+chsh -s /bin/zsh
