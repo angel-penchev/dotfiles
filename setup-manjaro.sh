c0c9fb34da2668b3d9675b6219a824894e8be84e
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2021-07-27 22:47:23 +0300

    [Feat] Manjaro vscode, spicetify, jenv and flutter
    Added configuration for all the above mentioned.
    Removed eslint as I am moving to per-project configurations.

--- setup-manjaro.sh
+++ setup-manjaro.sh
@@ -9,6 +9,7 @@ ln -sf "$(pwd)/common/git/.gitconfig" "$HOME/.gitconfig"
 #*zsh
 # Install oh-my-zsh and powerlevel10k
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm -rf ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --depth=1 "https://github.com/romkatv/powerlevel10k.git" ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Configure zsh
@@ -18,11 +19,17 @@ ln -sf "$(pwd)/common/zsh/.p10k.zsh" "$HOME/.p10k.zsh"
 chsh -s /bin/zsh
 
 
-#*nvm
-# Remove preinstalled node version
-pacman -U --ignore-dependencies node
-pacman -U --force node
+#*vscode
+# Configure vscode
+ln -sf "$(pwd)/common/vscode/settings.json" "$HOME/.config/Code/User/settings.json"
+ln -sf "$(pwd)/common/vscode/keybindings.json" "$HOME/.config/Code/User/keybindings.json"
+ln -sfF "$(pwd)/common/vscode/snippets" "$HOME/.config/Code/User/snippets"
 
+# Install extensions
+sh ./common/vscode/extensions.sh
+
+
+#*nvm
 # Install LTS version of node
 mkdir ~/.nvm
 nvm install --lts
@@ -32,11 +39,34 @@ nvm use --lts
 npm install -g npm
 
 
-#*eslint
-# Install eslint
-npm i -g eslint
-(cd "$HOME" ; npm i eslint-plugin-react @typescript-eslint/eslint-plugin eslint-config-google @typescript-eslint/parser)
-rm "$HOME/package*"
+#*jenv
+# Configure jenv with maven
+jenv enable-plugin export
+jenv enable-plugin maven
+jenv global 1.8
 
-# Configure eslint
-ln -sf "$(pwd)/common/eslint/.eslintrc.json" "$HOME/.eslintrc.json"
+# Register openjdk{8, 11}
+jenv add /usr/lib/jvm/java-8-openjdk/jre/
+jenv add /usr/lib/jvm/java-11-openjdk/
+
+
+#*flutter
+# Accept android licenses
+flutter doctor --android-license
+
+
+#*spicetify
+# Copy themes and extensions
+ln -sfF "$(pwd)/common/spicetify/Themes" "$HOME/.config/spicetify/Themes"
+ln -sfF "$(pwd)/common/spicetify/Extensions" "$HOME/.config/spicetify/Extensions"
+
+# Initialize and backup theme
+spicetify
+spicetify backup apply enable-devtool
+
+# Update spotify theme
+spicetify restore
+spicetify config extensions dribbblish.js
+spicetify config current_theme Dribbblish
+spicetify config color_scheme red-ocean
+spicetify apply
