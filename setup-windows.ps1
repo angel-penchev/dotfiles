3153d4462bb5f5ecb0cce93301570fd630c790b3
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2021-09-02 13:38:21 +0300

    [Feat] WSL setup + VSCode settings fix

--- setup-windows.ps1
+++ setup-windows.ps1
@@ -1,7 +1,20 @@
+#*wsl
+# Enable WSL
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+# Install WSL version 2
+Invoke-WebRequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile WSLUpdate.msi -UseBasicParsing
+msiexec.exe /package WSLUpdate.msi /quiet
+wsl --set-default-version 2
+
+
 #*code
-Remove-Item "$Env:USERPROFILE\AppData\Roaming\Code\User\settings.json"
-Remove-Item "$Env:USERPROFILE\AppData\Roaming\Code\User\keybindings.json"
-Remove-Item "$Env:USERPROFILE\AppData\Roaming\Code\User\snippets"
-cmd /c mklink /j "$Env:USERPROFILE\AppData\Roaming\Code\User\settings.json" "$PSScriptRoot\common\vscode\settings.json"
-cmd /c mklink /j "$Env:USERPROFILE\AppData\Roaming\Code\User\keybindings.json" "$PSScriptRoot\common\vscode\keybindings.json"
-cmd /c mklink /j /d "$Env:USERPROFILE\AppData\Roaming\Code\User\snippets" "$PSScriptRoot\common\vscode\snippets"
+Remove-Item -Force -ErrorAction SilentlyContinue "${Env:USERPROFILE}\AppData\Roaming\Code\User\settings.json"
+Remove-Item -Force -ErrorAction SilentlyContinue "${Env:USERPROFILE}\AppData\Roaming\Code\User\keybindings.json"
+Remove-Item -Force -Recurse -ErrorAction SilentlyContinue "${Env:USERPROFILE}\AppData\Roaming\Code\User\snippets"
+cmd /c mklink "${Env:USERPROFILE}\AppData\Roaming\Code\User\settings.json" "$PSScriptRoot\common\vscode\settings.json"
+cmd /c mklink "${Env:USERPROFILE}\AppData\Roaming\Code\User\keybindings.json" "$PSScriptRoot\common\vscode\keybindings.json"
+cmd /c mklink /j "${Env:USERPROFILE}\AppData\Roaming\Code\User\snippets" "$PSScriptRoot\common\vscode\snippets"
+& "$PSScriptRoot\common\vscode\extensions.ps1"
